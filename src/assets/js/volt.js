8e2295f5a45b5d58e01c9c96689f3c689933ea1f
Zoltán Szőgyényi <zoltan.szogyenyi@gmail.com>
2021-01-31 16:39:21 +0200

    Use local storage ro remember theme settings toggle.

--- src/assets/js/volt.js
+++ src/assets/js/volt.js
@@ -31,17 +31,28 @@ d.addEventListener("DOMContentLoaded", function(event) {
     var themeSettingsExpandEl = document.getElementById('theme-settings-expand');
 
     if(themeSettingsEl) {
+
         var themeSettingsCollapse = new bootstrap.Collapse(themeSettingsEl, {
             show: true,
             toggle: false
         });
+
+        if (window.localStorage.getItem('settings_expanded') === 'true') {
+            themeSettingsCollapse.show();
+            themeSettingsExpandEl.classList.remove('show');
+        } else {
+            themeSettingsCollapse.hide();
+            themeSettingsExpandEl.classList.add('show');
+        }
         
         themeSettingsEl.addEventListener('hidden.bs.collapse', function () {
             themeSettingsExpandEl.classList.add('show');
+            window.localStorage.setItem('settings_expanded', false);
         });
 
         themeSettingsExpandEl.addEventListener('click', function () {
             themeSettingsExpandEl.classList.remove('show');
+            window.localStorage.setItem('settings_expanded', true);
             setTimeout(function() {
                 themeSettingsCollapse.show();
             }, 300);
